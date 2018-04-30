9ea4334662f97f5746689651a480dfbe94a142b6
jlehocz <jozsef@lehocz.hu>
2018-04-30 21:26:57 +0200

    +tab

---
+++ app/src/main/java/com/example/android/miwok/PageFragmentAdapter.java
@@ -0,0 +1,33 @@
+package com.example.android.miwok;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class PageFragmentAdapter extends FragmentPagerAdapter {
+    final int PAGE_COUNT = 4;
+    private String tabTitles[] = new String[] { "Colors", "Family", "Numbers","Phrases" };
+    private Context context;
+
+    public PageFragmentAdapter(FragmentManager fm, Context context) {
+        super(fm);
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return PAGE_COUNT;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return PageFragment.newInstance(position + 1);
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        // Generate title based on item position
+        return tabTitles[position];
+    }
+}
